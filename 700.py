a1ddda6b9fbc158301a1903a38d8e8c86224a5f5
Max xu <666maxxxu@gmail.com>
2021-02-11 07:33:54 -0500

    Solution for Search in a Binary Search Treeeeeeeeeee

---
+++ 700.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def searchBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        if not root:
+	    return None
+		
+        while root:
+	    if root.val == val:
+    		return root
+	    elif root.val < val:
+    		root = root.right
+	    elif root.val > val:
+		root = root.left
