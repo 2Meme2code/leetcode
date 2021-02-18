824a0b6b9398114a2050f205267c523bc774675a
Max xu <666maxxxu@gmail.com>
2021-02-18 08:16:31 -0500

    Solution for N-ary Tree Preorder Traversal

---
+++ 589.py
@@ -0,0 +1,21 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution(object):
+    def preorder(self, root):
+        """
+        :type root: Node
+        :rtype: List[int]
+        """
+        if root == None: 
+            return []
+        output = [root.val]
+        for i in root.children: 
+            a = self.preorder(i)
+            output += a
+        return output
